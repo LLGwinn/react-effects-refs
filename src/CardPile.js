343bd4dd33dc2481d65202e23229eee05c8e6c04
Laura Lee Gwinn <ells.gwinn@gmail.com>
2022-02-15 15:46:01 -0500

    Part 2 complete

--- src/CardPile.js
+++ src/CardPile.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 import './CardPile.css';
 import Card from './Card';
@@ -6,7 +6,10 @@ import Card from './Card';
 function CardPile () {
     const [deck, setDeck] = useState(null);
     const [card, setCard] = useState(null);
+    const [autoDeal, setAutoDeal] = useState(false);
+    const intervalId = useRef();
 
+    // Gets a new shuffled deck one time
     useEffect(function getDeck() {
         async function fetchDeck() {
             const deckResult = await axios.get(
@@ -18,24 +21,47 @@ function CardPile () {
     }, []);
 
     function handleClick() {
-        async function fetchCard() {
-            const cardResult = await axios.get(
-                `http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
-            );
-            cardResult.data.remaining > 0 ?
-                setCard(cardResult.data.cards[0])
-                :
-                alert('Error: No cards remaining!')
-        }
-        fetchCard(); 
+        setAutoDeal(autoDeal => !autoDeal);
     }
+
+    useEffect(function startAutoDeal() {
+        // Gets a new card every time autoDeal state changes to true, every 1000 ms
+        if (autoDeal === true) {
+            intervalId.current = setInterval( () => {
+
+                async function fetchCard() {
+                        const cardResult = await axios.get(
+                            `http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
+                        );
+                        
+                        if (cardResult.data.remaining >= 0 && !cardResult.data.error) {
+                            setCard(cardResult.data.cards[0])
+                        } else {
+                            alert('Error: no cards remaining!');
+                            setAutoDeal(false); 
+                        }            
+                }
+
+                fetchCard();
+            }, 1000);
+        }
+
+        return function cleanUpInterval() {
+            clearInterval(intervalId.current);
+        };
+
+    }, [autoDeal]);
 
     return(
         <div>
             { card ?
                 <Card image={card.image }/> : null
-            } 
-            <button onClick={handleClick}>Deal Card</button>
+            }
+            { autoDeal ?
+                <button onClick={handleClick}>Stop!</button>
+                :
+                <button onClick={handleClick}>Start Dealing!</button>
+            }
         </div>
 
     )
